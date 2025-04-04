2f4486c65a18415e02bbf4d0d26b233e1e29de3f
Simon Facer <43856472+SimonF-MSFT@users.noreply.github.com>
2025-04-04 10:33:26 -0400

    20250404-5

--- MD-Link-Checker.ps1
+++ MD-Link-Checker.ps1
@@ -418,7 +418,7 @@ function Get-LinkType ([string]$URLToValidate, [string]$CurrFolder, [string]$Pub
     $FileToScan_FileType = $FileToScan_Parts[$FileToScan_Parts.count - 1]
     if ($FileToScan_FileType -ne "md")
       {
-        Write-Host "PS: File is not Markdown - Skipping"
+        Write-Host "PS: File is not Markdown ($FileToScan_FileType) - Skipping"
         exit 0
       }
 
